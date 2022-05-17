44ea31805c7e51da534a56108d9e3608ea09731f
ohmzs <38644134+ohmzs@users.noreply.github.com>
2022-05-17 10:46:43 -0400

    commit tree a549c58eaf56a7bf699963950f706c0d90c3cf65

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const colorH1 = {
+    color: 'firebrick'
+  };
+  return <div id="home">
+    <h1 style ={colorH1}>{name} is a Web Developer from {city}</h1>
+  </div>
 }
-
 export default Home;
